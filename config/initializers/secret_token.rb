cd44241e33ef0a10a41451db9c9efc3e79f3b6e5
mkitagawa <mkitagawa@terrasky.co.jp>
2013-10-07 13:21:14 +0900

    rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AttendanceBook::Application.config.secret_token = '484729ffb0ce6a56550ec3225f42a19ba8ff30077ca591c392a6b9f12647e84bffb555fedd62f31c2e9eb1b80cfa1aea01ac1da7ec63f525071a8856e879431d'
